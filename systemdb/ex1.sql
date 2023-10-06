36fc9324c083999d4765cf0c5f735b4b903b87f3
KyoungHwanJeong <jkh3100@naver.com>
2023-10-06 15:10:04 +0900

    sql source

---
+++ systemdb/ex1.sql
@@ -0,0 +1,19 @@
+-- 스키마:system
+-- 테이블
+CREATE TABLE ex1(
+    colum1 CHAR(10),    -- 고정길이 문자(10 Byte)
+    colum2 VARCHAR2(10) -- 가변길이 문자(10 Byte)
+    
+);
+
+-- 데이터 추가
+INSERT INTO ex1(colum1, colum2) VALUES ('abc', 'abc');  -- SQL은 ' '를 쓴다
+INSERT INTO ex1 VALUES ('당산', '당산');
+
+-- 데이터 조회
+SELECT colum1, LENGTH(colum1),colum2 ,LENGTH(colum2) FROM ex1;  -- LENGTH()함수: colum의 길이.
+
+-- 트랜잭션 : COMMIT, ROLLBACK
+COMMIT;
+
+DROP TABLE ex1;
