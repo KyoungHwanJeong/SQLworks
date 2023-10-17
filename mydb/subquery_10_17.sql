fde2730eca948715f8179cef115ecf12990c7d2b
KyoungHwanJeong <jkh3100@naver.com>
2023-10-17 15:15:02 +0900

    10_17

---
+++ mydb/subquery_10_17.sql
@@ -0,0 +1,89 @@
+-- 서브 쿼리(Sub Query): 부속질의(중첩 쿼리)
+-- SELECT문 내부에 또 SELECT문이 존재함
+
+-- 도서중 가격이 가장 높은 가격을 검색하시오
+-- MAX(price) : 35000
+SELECT MAX(price) FROM book;
+
+-- 도서중에서 가장 비싼 도서의 이름을 검색하시오
+SELECT bookname, price
+FROM book
+WHERE price = 
+      (SELECT MAX(price) FROM book);
+      
+-- 도서를 구매한 적이 있는 고객의 이름을 검색하시오
+SELECT custid FROM orders;
+
+/*SELECT name
+FROM customer
+WHERE custid IN(1, 2, 3, 4);*/
+
+SELECT name
+FROM customer
+WHERE custid IN(SELECT custid FROM orders);
+
+-- 다중행 서브쿼리 (WHERE 절)
+-- 도서를 구매한 적이 있는 고객의 이름 출력
+SELECT name
+FROM customer
+WHERE custid IN (SELECT custid FROM orders);
+
+-- 다중행 서브쿼리 (WHERE 절)
+-- 도서를 구매한 적이 없는 고객의 이름 출력
+SELECT name
+FROM customer
+WHERE custid NOT IN (SELECT custid FROM orders);
+
+-- 조인 방식 : customer, orders
+-- 도서를 구매한 적이 있는 고객의 이름을 검색하시오
+SELECT DISTINCT cus.name
+FROM customer cus, orders ord
+WHERE cus.custid = ord.custid;
+
+-- FROM절에 있는 서브쿼리 - 인라인뷰 라고도 함
+-- 고객의 이름과 고객의 판매액 검색(customer, orders)
+-- 그룹별 : Groub by
+-- 단 , 고객 번호가 2 이하임
+
+-- 동등 조인
+SELECT cs.name, SUM(od.saleprice) total
+FROM customer cs, orders od
+WHERE cs.custid = od.custid 
+    AND cs.custid <= 2
+GROUP BY cs.name;
+
+-- HAVING이 들어간...?
+SELECT cs.name, SUM(od.saleprice) total
+FROM customer cs, orders od
+WHERE cs.custid = od.custid 
+    AND cs.custid <= 2
+GROUP BY cs.name;
+    HAVING SUM(od.saleprice) >= 30000;
+/*
+ 실행순서
+ 1. FROM 절 테이블명
+ 2. WHERE, GROUP BY
+ 3. SELECT 절
+ 4. ORDER 절
+*/
+-- 서브 쿼리(FROM 절)
+SELECT cs.name, SUM(od.saleprice)
+FROM (SELECT * FROM customer
+        WHERE custid <= 2) cs, 
+     orders od
+WHERE cs.custid = od.custid
+GROUP BY cs.name;
+
+--
+SELECT * FROM product;
+SELECT * FROM product_review;
+
+-- 스칼라 서브쿼리
+--상품 리뷰 테이블에 있는 상품 이름 검색(SELECT 절)
+SELECT  a.product_code,
+        (select b.product_name 
+         from product b
+         where a.product_code = b.product_code) product_name,
+        a.member_id,
+        a.content
+FROM product_review a;
---
+++ mydb/summer_price_10_17.sql
@@ -0,0 +1,54 @@
+-- 수강료 테이블
+CREATE TABLE summer_price(
+    subject VARCHAR2(20) PRIMARY KEY,
+    price NUMBER
+);
+
+INSERT INTO summer_price VALUES ('C', 30000);
+INSERT INTO summer_price VALUES ('Java', 45000);
+INSERT INTO summer_price VALUES ('Python', 40000);
+
+-- 여름학기 등록 테이블
+CREATE TABLE summer_register(
+    sid     NUMBER PRIMARY KEY,
+    subject VARCHAR2(20),
+    FOREIGN KEY(subject) REFERENCES summer_price(subject)
+);
+
+INSERT INTO summer_register VALUES (100, 'C');
+INSERT INTO summer_register VALUES (101, 'Java');
+INSERT INTO summer_register VALUES (200, 'Python');
+INSERT INTO summer_register VALUES (201, 'Java');
+
+-- 200번 학생의 수강 신청 취소
+-- 삭제 이상 없음
+DELETE FROM summer_register
+WHERE sid = 200;
+
+-- Python 과목의 수강료는?
+SELECT price
+FROM summer_price
+WHERE subject = 'Python';
+
+-- 수강료 테이블에 'C++' 강좌를 35,000으로 삽입
+INSERT INTO summer_price VALUES('C++', 35000);      -- 삽입 이상 없음
+
+-- 수강 신청 정보 확인하기
+SELECT * FROM summer_price;
+
+-- Java 수강료가 45,000원에서 40,000원으로 변경
+UPDATE summer_price SET price = 40000               -- 수정 이상 없음
+WHERE subject LIKE 'Java';
+
+
+-- 수강료가 가장 싼 과목은?
+SELECT subject, price FROM summer_price
+WHERE price = (SELECT MIN(price)FROM summer_price);
+
+
+COMMIT;
+
+SELECT * FROM summer_register;
+
+drop table summer_price;
+drop table summer_register;
